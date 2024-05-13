aae5ac9373b2ca76e45061aad25618579f9f0072
Lucassis7 <lucassis7@outlook.com>
2024-05-13 17:09:48 -0300

    rendering the button component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import { useState } from "react";
+import Button from "./components/Button";
 import "./App.css";
+import listIcon from "./assets/list-icon.svg";
+import mapIcon from "./assets/map-icon.svg";
+import phoneIcon from "./assets/phone-icon.svg";
+import gearIcon from "./assets/gear-icon.svg";
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <main>
+        {/* Hero container */}
+        <div></div>
+
+        {/* Option cards container */}
+        <div></div>
+
+        {/* Footer buttons container */}
+        <div className="buttons">
+          <Button buttonName="Lista" icon={listIcon} />
+          <Button buttonName="Mapa" icon={mapIcon} />
+          <Button buttonName="Suporte" icon={phoneIcon} />
+          <Button buttonName="Ajustes" icon={gearIcon} />
+        </div>
+      </main>
     </>
   );
 }
