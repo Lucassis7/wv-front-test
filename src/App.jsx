8adb5a083ee974085219fb9b22f3dae0cb5c54cd
Lucassis7 <lucassis7@outlook.com>
2024-05-13 17:16:15 -0300

    feat: rendering the Card component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import Card from "./components/Card";
 import Button from "./components/Button";
 import "./App.css";
+import clipboardIcon from "./assets/clipboard-icon.svg";
+import buildingIcon from "./assets/building-icon.svg";
 import listIcon from "./assets/list-icon.svg";
 import mapIcon from "./assets/map-icon.svg";
 import phoneIcon from "./assets/phone-icon.svg";
@@ -14,7 +17,13 @@ function App() {
         <div></div>
 
         {/* Option cards container */}
-        <div></div>
+        <div>
+          <h1>Como ficam meus benefícios?</h1>
+          <div className="cards">
+            <Card cardName="Plano de Saúde" icon={clipboardIcon} />
+            <Card cardName="Previdência Privada" icon={buildingIcon} />
+          </div>
+        </div>
 
         {/* Footer buttons container */}
         <div className="buttons">
